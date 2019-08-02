2d7e3640af4281e971259368cd038cb58d484569
Rahul Upakare <rahulu@gmail.com>
2019-08-02 09:29:49 +0530

    concise

--- src/hello_switch_falltrough/hello_switch_fallthrough.go
+++ src/hello_switch_falltrough/hello_switch_fallthrough.go
@@ -5,11 +5,8 @@ import "fmt"
 func main() {
 	i := 42
 	switch i {
-	case 0:
-		fmt.Print("case 0 doesn't match")
-		fallthrough
 	case 1:
-		fmt.Print("case 1 doesn't match")
+		fmt.Print("case 1")
 		fallthrough
 	case 42:
 		fmt.Print("Hello, ")
@@ -20,7 +17,7 @@ func main() {
 	case 404:
 		fmt.Println("界")
 	case 440:
-		fmt.Println("case doesn't match and no fallthrough above")
+		fmt.Println("case 440")
 	default:
 		fmt.Println("default")
 	}
